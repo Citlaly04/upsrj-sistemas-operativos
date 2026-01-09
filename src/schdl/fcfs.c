1924541198078bddfb9d91c8fac30e700af47a9c
Citlaly04-afk <citlalyzape@gmail.com>
2026-01-08 21:58:45 -0600

    entrega fcfs

--- src/schdl/fcfs.c
+++ src/schdl/fcfs.c
@@ -11,7 +11,28 @@ int main() {
     init_processes(p, n);
 
     // TODO: Aquí va la lógica del scheduler
-    
+    in time=0;
     print_results(p, n, "FCFS Scheduling");
     return 0;
+
+     // FCFS: ejecutar en orden de llegada
+    for (int i = 0; i < n; i++) {
+        if (time < p[i].arrival_time) {;
+            // Si el CPU está ocioso, avanzar hasta la llegada del proceso
+            time = p[i].arrival_time;
+        }
+
+        // Mostrar estado antes de ejecutar
+        printf("Tiempo %d: Ejecutando P%d (BT=%d)\n",
+               time, p[i].id, p[i].burst_time);
+
+        // Calcular tiempos
+        p[i].waiting_time = time - p[i].arrival_time;
+        time += p[i].burst_time;
+        p[i].turnaround_time = p[i].waiting_time + p[i].burst_time;
+        p[i].completed = 1;
+
+        // Mostrar estado después de ejecutar
+        printf("   -> P%d terminó en tiempo %d\n", p[i].id, time);
+    }
 }
